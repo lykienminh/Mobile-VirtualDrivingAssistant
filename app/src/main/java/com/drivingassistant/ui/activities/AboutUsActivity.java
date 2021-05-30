a638261bc4360a487de76892a53f02d4320b035d
ngocquy25 <quy.nguyen.jirou@gmail.com>
2021-05-30 11:31:11 +0700

    update about us + traffic situtation

---
+++ app/src/main/java/com/drivingassistant/ui/activities/AboutUsActivity.java
@@ -0,0 +1,23 @@
+package com.drivingassistant.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.drivingassistant.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
+
+    public void returnHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
